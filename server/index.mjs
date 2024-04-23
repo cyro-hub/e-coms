98d781e7966848110efaba9429420f4a6b991d56
cyro-hub <cyrocyro2@gmail.com>
2024-04-23 20:20:52 +0300

    updated the package.json file

--- server/index.mjs
+++ server/index.mjs
@@ -6,6 +6,7 @@ import "./config/database/dbconfig.mjs";
 import cookieParser from "cookie-parser";
 import path from "path";
 import routes from "./routes/routes.mjs";
+import bodyParser from "body-parser";
 
 dotenv.config();
 
@@ -13,8 +14,11 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use(cookieParser());
 app.use("/api", routes);
